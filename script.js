81121d9bec273dac9f151fce5706a31c6def8835
polyalyameow <117765092+polyalyameow@users.noreply.github.com>
2023-08-31 22:33:02 +0200

    script to show only x amount of articles at a time + new db in progress

--- script.js
+++ script.js
@@ -17,51 +17,39 @@ http.onload = function () {
     // empty var to add the incoming data
     let output = "";
 
-    // for (item of news) {
-
-    //   // console.log(item)
-    //   // console.log(news)
-      
-
-    //   output += `
-    //             <div class="articles--other">
-    //                 <div class="articles--other__container">
-    //                     <img  src="${item.image}" alt="${item.alt}" class="articles--other__pic" />
-    //                     <h2 class="articles--other__title">${item.title}</h2>
-    //                     <p class="articles--other__description">${item.description}</p>
-    //                 </div>
-    //             </div>
-    //         `;
-    // }
-
-      let varCheck = [];
-      for (let item in news){
-        
+    let varCheck = [];
+    let i = 2;
+    
+   
+      for (let item in news) {
         let random = Math.floor(Math.random() * news.length);
         let random_news = news[random];
         if (varCheck.includes(random_news.title)) {
-          console.log("already exist")
-        } else {
-          varCheck.push(random_news.title);
-          // varCheck.push(JSON.stringify(random_news));
-          // console.log(Object.values(random_news));
-          // console.log(Object.values(random_news));
-          // console.log({random_news})
-          console.log(random_news.title)
-          output += `
-                    <div class="articles--other">
-                        <div class="articles--other__container">
-                          <img  src="${random_news.image}" alt="${random_news.alt}" class="articles--other__pic" />
-                            <h2 class="articles--other__title">${random_news.title}</h2>
-                            <p class="articles--other__description">${random_news.description}</p>
+          console.log("already exist");
+        } else if (varCheck.length < 9) {
+        
+            
+            varCheck.push(random_news.title);
+            output += `
+                      <div class="articles--other">
+                          <div class="articles--other__container">
+                            <img  src="${random_news.image}" alt="${random_news.alt}" class="articles--other__pic" />
+                              <h2 class="articles--other__title">${random_news.title}</h2>
+                              <p class="articles--other__description">${random_news.description}</p>
+                        </div>
                       </div>
-                    </div>
-                `;
-                
-
-        }
+                  `;
+          
+                }
         document.querySelector(".articles--other").innerHTML = output;
-      }
+      
+      
+    }
       console.log(varCheck);
-  }
-}
+    }
+      
+      
+
+    
+  
+};
