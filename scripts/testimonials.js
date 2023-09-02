30ffadbc36f06cf9101affcd1c4edcbcbedd3055
polyalyameow <117765092+polyalyameow@users.noreply.github.com>
2023-09-02 16:01:57 +0200

    testimonials db

---
+++ scripts/testimonials.js
@@ -0,0 +1,32 @@
+
+
+async function fetchTestimonials() {
+    await 
+    fetch("/public/testimonials.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        let output = "";
+
+        for (item in data) {
+            console.log(data);
+            
+    
+              output += `
+                <div class="aside"> 
+                    <img class="aside__pic" src="${data[item].image}" alt="${data[item].alt}"/>
+                    <p class="aside__caption-name">${data[item].name} ${data[item].surname}</p>
+                    <p class="aside__caption-city">${data[item].city}</p>
+                    <p class="aside__text">${data[item].text}</p>
+                </div>
+    
+         ` ; 
+    
+         
+          }
+
+          document.querySelector(".aside").innerHTML = output;
+        });
+    };
+    
+    fetchTestimonials()
