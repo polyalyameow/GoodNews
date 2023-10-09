836faaa41120517271de44212b5f95a5f73c1268
polyalyameow <117765092+polyalyameow@users.noreply.github.com>
2023-10-09 21:58:47 +0200

    fixed paths

--- scripts/articleFilter.js
+++ scripts/articleFilter.js
@@ -256,13 +256,13 @@ window.onload = function fetchData() {
       }
 
       for (let news in newsAll) {
-        if (currentUrl === "https://master--melodious-otter-3f6d38.netlify.app/pages/culture") {
+        if (currentUrl === "https://melodious-otter-3f6d38.netlify.app/pages/culture") {
           document.querySelector(".culture__articles").innerHTML = result;
-        } else if (currentUrl === "https://master--melodious-otter-3f6d38.netlify.app/pages/politics") {
+        } else if (currentUrl === "https://melodious-otter-3f6d38.netlify.app/pages/politics") {
           document.querySelector(".politics__articles").innerHTML = result;
-        } else if (currentUrl === "https://master--melodious-otter-3f6d38.netlify.app/pages/world") {
+        } else if (currentUrl === "https://melodious-otter-3f6d38.netlify.app/pages/world") {
           document.querySelector(".world__articles").innerHTML = result;
-        } else if (currentUrl === "https://master--melodious-otter-3f6d38.netlify.app/pages/health") {
+        } else if (currentUrl === "https://melodious-otter-3f6d38.netlify.app/pages/health") {
           document.querySelector(".health__articles").innerHTML = result;
         }
       }
